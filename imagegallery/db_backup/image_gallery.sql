ba9fdecbaec38f16ea267f144a678cc73d2d8572
Supriya Patra <supr.nh@gmail.com>
2023-06-19 02:51:45 +0530

    db upload

---
+++ imagegallery/db_backup/image_gallery.sql
@@ -0,0 +1,432 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 18, 2023 at 11:19 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `image_gallery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_groups_users`
+--
+
+CREATE TABLE `auth_groups_users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_identities`
+--
+
+CREATE TABLE `auth_identities` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `secret` varchar(255) NOT NULL,
+  `secret2` varchar(255) DEFAULT NULL,
+  `expires` datetime DEFAULT NULL,
+  `extra` text DEFAULT NULL,
+  `force_reset` tinyint(1) NOT NULL DEFAULT 0,
+  `last_used_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_logins`
+--
+
+CREATE TABLE `auth_logins` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `ip_address` varchar(255) NOT NULL,
+  `user_agent` varchar(255) DEFAULT NULL,
+  `id_type` varchar(255) NOT NULL,
+  `identifier` varchar(255) NOT NULL,
+  `user_id` int(11) UNSIGNED DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `success` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_permissions_users`
+--
+
+CREATE TABLE `auth_permissions_users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `permission` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_remember_tokens`
+--
+
+CREATE TABLE `auth_remember_tokens` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `selector` varchar(255) NOT NULL,
+  `hashedValidator` varchar(255) NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `expires` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_token_logins`
+--
+
+CREATE TABLE `auth_token_logins` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `ip_address` varchar(255) NOT NULL,
+  `user_agent` varchar(255) DEFAULT NULL,
+  `id_type` varchar(255) NOT NULL,
+  `identifier` varchar(255) NOT NULL,
+  `user_id` int(11) UNSIGNED DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `success` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `category_id`, `name`, `created_at`, `updated_at`) VALUES
+(2, 3, 'Ak.jpg', '2023-06-15 05:45:19', '2023-06-15 05:45:19'),
+(3, 2, 'bird1.png', '2023-06-15 06:33:59', '2023-06-15 06:33:59'),
+(4, 2, 'boris-smokrovic-RLLR0oRz16Y-unsplash.jpg', '2023-06-15 06:34:06', '2023-06-15 06:34:06'),
+(5, 2, 'boris-smokrovic-T_diWdiLbg0-unsplash.jpg', '2023-06-15 06:34:11', '2023-06-15 06:34:11'),
+(6, 2, 'pexels-andrew-mckie-2400030.jpg', '2023-06-15 06:34:32', '2023-06-15 06:34:32'),
+(7, 1, 'autumn-fall-nature-scene-autumnal-park-beautiful-77869343.jpg', '2023-06-15 06:35:54', '2023-06-15 06:35:54'),
+(8, 1, 'Cool-Nature-Background-620x349.jpg', '2023-06-15 06:36:10', '2023-06-15 06:36:10'),
+(9, 1, 'Flower-HD-Wallpaper-Nature-620x349.jpg', '2023-06-15 06:36:19', '2023-06-15 06:36:19'),
+(10, 1, 'nature-calls.jpg', '2023-06-15 06:36:31', '2023-06-15 06:36:31'),
+(11, 1, 'Nature-Desktop-Wallpaper-620x349.jpg', '2023-06-15 06:36:48', '2023-06-15 06:36:48'),
+(12, 3, 'beautiful-pic-of-flower-free-download.jpg', '2023-06-15 06:37:36', '2023-06-15 06:37:36'),
+(13, 3, 'beautiful-rose-pic-download-for-girlfriend.jpg', '2023-06-15 06:37:42', '2023-06-15 06:37:42'),
+(14, 3, 'nice-red-flower-download-for-fb.jpg', '2023-06-15 06:39:10', '2023-06-15 06:39:10'),
+(15, 3, 'new-pic-of-flowers-download-for-Whatsapp.jpg', '2023-06-15 06:39:19', '2023-06-15 06:39:19'),
+(16, 3, 'beautiful-wallpaper-of-flower-download.jpg', '2023-06-15 06:39:28', '2023-06-15 06:39:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `img_categories`
+--
+
+CREATE TABLE `img_categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(150) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `img_categories`
+--
+
+INSERT INTO `img_categories` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'nature', 'This category for add nature image', '2023-06-14 14:36:28', '2023-06-14 14:36:28'),
+(2, 'bird', 'birds image will add here', '2023-06-14 15:15:50', '2023-06-15 14:39:14'),
+(3, 'flower', 'flower image will add here', '2023-06-14 15:16:39', '2023-06-14 15:16:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
+(1, '2020-12-28-223112', 'CodeIgniter\\Shield\\Database\\Migrations\\CreateAuthTables', 'default', 'CodeIgniter\\Shield', 1687113743, 1),
+(2, '2021-07-04-041948', 'CodeIgniter\\Settings\\Database\\Migrations\\CreateSettingsTable', 'default', 'CodeIgniter\\Settings', 1687113743, 1),
+(3, '2021-11-14-143905', 'CodeIgniter\\Settings\\Database\\Migrations\\AddContextColumn', 'default', 'CodeIgniter\\Settings', 1687113743, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int(9) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `value` text DEFAULT NULL,
+  `type` varchar(31) NOT NULL DEFAULT 'string',
+  `context` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `status_message` varchar(255) DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 0,
+  `last_active` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `status`, `status_message`, `active`, `last_active`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, NULL, 'supriya', 'patra', 'supr.nhc@gmail.com', '$2y$10$3LwhidoNDhmfaas97REuLOaRIaSz.ssYUapyPkZHli0kFYSYyIQgS', NULL, NULL, 0, NULL, NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `auth_groups_users`
+--
+ALTER TABLE `auth_groups_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `auth_groups_users_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `auth_identities`
+--
+ALTER TABLE `auth_identities`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `type_secret` (`type`,`secret`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `auth_logins`
+--
+ALTER TABLE `auth_logins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_type_identifier` (`id_type`,`identifier`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `auth_permissions_users`
+--
+ALTER TABLE `auth_permissions_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `auth_permissions_users_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `auth_remember_tokens`
+--
+ALTER TABLE `auth_remember_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `selector` (`selector`),
+  ADD KEY `auth_remember_tokens_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `auth_token_logins`
+--
+ALTER TABLE `auth_token_logins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_type_identifier` (`id_type`,`identifier`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `img_categories`
+--
+ALTER TABLE `img_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `auth_groups_users`
+--
+ALTER TABLE `auth_groups_users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_identities`
+--
+ALTER TABLE `auth_identities`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_logins`
+--
+ALTER TABLE `auth_logins`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_permissions_users`
+--
+ALTER TABLE `auth_permissions_users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_remember_tokens`
+--
+ALTER TABLE `auth_remember_tokens`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_token_logins`
+--
+ALTER TABLE `auth_token_logins`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `img_categories`
+--
+ALTER TABLE `img_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `auth_groups_users`
+--
+ALTER TABLE `auth_groups_users`
+  ADD CONSTRAINT `auth_groups_users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `auth_identities`
+--
+ALTER TABLE `auth_identities`
+  ADD CONSTRAINT `auth_identities_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `auth_permissions_users`
+--
+ALTER TABLE `auth_permissions_users`
+  ADD CONSTRAINT `auth_permissions_users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `auth_remember_tokens`
+--
+ALTER TABLE `auth_remember_tokens`
+  ADD CONSTRAINT `auth_remember_tokens_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
